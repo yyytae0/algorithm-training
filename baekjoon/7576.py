52d19e32ff77d27595948d83431b98f101d08196
yyytae0 <yyytae0@gmail.com>
2023-02-07 17:16:25 +0900

    bfs dfs 위주

--- baekjoon/7576.py
+++ baekjoon/7576.py
@@ -1,22 +1,25 @@
-def bfs(a, b):
+from collections import deque
+
+
+def bfs():
     way = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-    queue = [[a, b]]
-    visit[a][b] = 1
     while queue:
-        v = queue[0]
+        v = queue.popleft()
         cnt = ans[v[0]][v[1]]
-        del queue[0]
         for i in way:
             new_v = [v[0] + i[0], v[1] + i[1]]
             if 0 <= new_v[0] <= m-1 and 0 <= new_v[1] <= n-1:
                 if visit[new_v[0]][new_v[1]] == 0 and lst[new_v[0]][new_v[1]] != -1 and lst[new_v[0]][new_v[1]] != 1:
                     visit[new_v[0]][new_v[1]] = 1
+                    # if lst[new_v[0]][new_v[1]] == 1:
+                    #     queue = []
+                    #     break
                     if ans[new_v[0]][new_v[1]] == 0:
                         ans[new_v[0]][new_v[1]] = cnt+1
                         queue.append(new_v)
                     else:
-                        if cnt+1 >= ans[new_v[0]][new_v[1]]:
-                            pass
+                        if cnt+1 > ans[new_v[0]][new_v[1]]:
+                            break
                         else:
                             ans[new_v[0]][new_v[1]] = cnt+1
                             queue.append(new_v)
@@ -25,14 +28,31 @@ def bfs(a, b):
 n, m = map(int, input().split())
 lst = list(list(map(int, input().split())) for _ in range(m))
 ans = [[0 for _ in range(n)] for _ in range(m)]
+queue = deque()
 for i in range(m):
     for j in range(n):
         if lst[i][j] == 1:
-            visit = [[0 for _ in range(n)] for _ in range(m)]
             ans[i][j] = 1
-            bfs(i, j)
+            queue.append([i, j])
 
         if lst[i][j] == -1:
             ans[i][j] = -1
 
-for i in
+visit = [[0 for _ in range(n)] for _ in range(m)]
+bfs()
+mx = 0
+dummy = 0
+for i in range(m):
+    if dummy == 1:
+        break
+    for j in range(n):
+        if ans[i][j] == 0:
+            mx = 0
+            dummy = 1
+            break
+
+        else:
+            if ans[i][j] > mx:
+                mx = ans[i][j]
+
+print(mx-1)
---
+++ train.py
@@ -0,0 +1,5 @@
+from collections import deque
+
+a = [1, 2]
+queue = deque(a)
+print(deque.popleft(queue))
