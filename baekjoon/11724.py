b4a78e6e83c582e38933688ee15d649d3308eebd
yyytae0 <yyytae0@gmail.com>
2023-02-07 17:47:00 +0900

    bfs master

--- baekjoon/11724.py
+++ baekjoon/11724.py
@@ -1,14 +1,30 @@
 from sys import stdin
+from collections import deque
+
+
+def bfs(a):
+    q = deque()
+    q.append(a)
+    while q:
+        v = q.popleft()
+        for i in range(1, n+1):
+            if link[v][i] == 1 and visit[i] == 0:
+                q.append(i)
+                visit[i] = 1
+
 
 n, m = map(int, stdin.readline().split())
-lst = list()
+link = [[0 for _ in range(n+1)] for _ in range(n+1)]
+visit = [0 for _ in range(n+1)]
 for i in range(m):
-    lst.append(list(map(int, stdin.readline().split())))
+    a, b = map(int, stdin.readline().split())
+    link[a][b] = 1
+    link[b][a] = 1
 
-print(lst)
-while len(lst) != 0:
-    dummy = set(lst.pop(0))
-    for i in lst:
-        if (i[0] in dummy) or (i[1] in dummy):
-            dummy = dummy.union({i[0], i[1]})
+cnt = 0
+for i in range(1, n+1):
+    if visit[i] == 0:
+        bfs(i)
+        cnt += 1
 
+print(cnt)
--- baekjoon/1697.py
+++ baekjoon/1697.py
@@ -1,37 +1,22 @@
-def find(start, arrive):
-    if start == arrive:
-        return True
-
-    elif arrive % 2 == 1:
-        find(start, arrive + 1)
-        find(start, arrive - 1)
-
-    else:
-        if start <= (arrive/2):
-            find(start, arrive/2)
-
-        else:
-            pass
+from collections import deque
 
 
-# ip = list(map(int, input().split()))
-# now = ip[0]
-# cnt = 0
-#
-# while True:
-#     if now * 2 > ip[1]:
-#         if now*2-ip[1] > ip[1]-now:
-#             cnt = cnt + ip[1] - now
-#
-#         else:
-#             cnt = cnt + now*2 - ip[1] + 1
-#         break
-#
-#     elif now == ip[1]:
-#         break
-#
-#     else:
-#         now = now * 2
-#         cnt += 1
-#
-# print(cnt)
+def bfs():
+    q = deque()
+    q.append(n)
+    while q:
+        v = q.popleft()
+        if v == k:
+            break
+
+        for i in [v-1, v+1, v*2]:
+            if 0 <= i <= mx and not lst[i]:
+                lst[i] = lst[v] + 1
+                q.append(i)
+
+
+n, k = map(int, input().split())
+mx = 10**5
+lst = [0 for _ in range(mx+1)]
+bfs()
+print(lst[k])
