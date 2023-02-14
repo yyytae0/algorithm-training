269f050cc01c785dea8f79422e50e7ee948084ba
yyytae0 <yyytae0@gmail.com>
2023-02-14 17:47:42 +0900

    알고리즘

--- train.py
+++
@@ -1,8 +0,0 @@
-ip = int(input())
-
-for case in range(1, ip+1):
-    n = int(input())
-    dp = [1 for _ in range((n//10)+1)]
-    for i in range(2, (n//10)+1):
-        dp[i] = dp[i-1] + 2*dp[i-2]
-    print(f'#{case}', dp[n//10])
