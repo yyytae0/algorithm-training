8c5a6b0dbbec63b32a4c1785344ab85d67eb3897
yyytae0 <yyytae0@gmail.com>
2023-02-17 16:14:48 +0900

    1072 이진탐색으로 해결

--- baekjoon/1072.py
+++ baekjoon/1072.py
@@ -1,43 +1,29 @@
-def check(a, b):
-    cnt = 0
-    for i in range(len(a)):
-        for j in range(len(a[0])):
-            if a[i][j] != b[i][j]:
-                if (i+2) >= len(a) or (j+2) >= len(a[0]):
-                    return -1
-
-                else:
-                    cnt += 1
-                    for k in range(3):
-                        for l in range(3):
-                            a[i + k][j + l] = str(1 - int(a[i + k][j + l]))
-
-    if a == b:
-        return cnt
-
-    else:
-        return -1
+# def check(a, b):
+#     if 99*a <= 100*b:
+#         return -1
+#     else:
+#         if a**2 % (99*a-100*b):
+#             return a**2//(99*a-100*b) + 1
+#         else:
+#             return a**2//(99*a-100*b)
+#
+#
+# a, b = map(int, input().split())
+# ans = check(a, b)
+# print(ans)
 
 
-ip = list(map(int, input().split()))
-a = []
-b = []
-
-for i in range(ip[0]):
-    lst = list(input())
-    a.append(lst)
-
-for i in range(ip[0]):
-    lst = list(input())
-    b.append(lst)
-
-
-if a == b:
-    print(0)
-
-elif ip[0] < 3 or ip[1] < 3:
+a, b = map(int, input().split())
+p = (b*100)//a
+if p >= 99:
     print(-1)
-
 else:
-    ans = check(a, b)
-    print(ans)
+    left = 1
+    right = a
+    while left <= right:
+        mid = (left + right)//2
+        if ((b+mid)*100)//(a+mid) > p:
+            right = mid - 1
+        else:
+            left = mid + 1
+    print(left)
--- baekjoon/10830.py
+++ baekjoon/10830.py
@@ -1,10 +1,33 @@
+def multi(a, b):
+    d = [[0 for _ in range(n)] for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                d[i][j] = (d[i][j] + ((a[i][k]%1000) * (b[k][j]%1000)) % 1000) % 1000
+    return d
+
+
 def solve(a, b):
+    d = [[0 for _ in range(n)] for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                d[i][j] = (d[i][j] + ((a[i][k]%1000) * (a[k][j]%1000)) % 1000) % 1000
+
+    if b == 2:
+        return d
+    if b == 1:
+        return a
     if b % 2:
-        return solve(d, b//2) * solve(d, b//2) * a
+        return multi(solve(d, b//2), a)
     else:
-        return solve(d, b//2) * solve(d, b//2)
+        return solve(d, b//2)
 
 
 n, m = map(int, input().split())
 lst = list(list(map(int, input().split())) for _ in range(n))
-solve(lst, m)
+ans = solve(lst, m)
+for i in ans:
+    for j in i:
+        print(j%1000, end=' ')
+    print()
