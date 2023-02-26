47cb5a75c7a7d365484fc5820a843b99601d5419
yyytae0 <yyytae0@gmail.com>
2023-02-26 16:39:18 +0900

    230226 start

--- dfs.py
+++ dfs.py
@@ -1,15 +1,17 @@
 def dfs(n, m):
     global s
-    global li
-    if len(s) == m:
-        li.append(s[:])
-        return
+    # global li
+    # if len(s) == m:
+    #     li.append(s[:])
+    #     return
 
     for i in range(1, n+1):
         if i not in s:
             s.append(i)
+
             dfs(n, m)
             s.pop()
+
 
 
 n, m = list(map(int, input().split()))
