be6ed402f633d02f3d5bd20d15c574ecc3792a8d
yyytae0 <yyytae0@gmail.com>
2023-02-27 13:49:19 +0900

    230227

--- baekjoon/1091.py
+++ baekjoon/1091.py
@@ -1,3 +1,28 @@
+def change():
+    new = ['' for _ in range(n)]
+    a = 1
+    for i in range(n):
+        new[rule[i]] = now[i]
+        if ans[rule[i]] != now[i]:
+            a = 0
+    return a, new
+
+
 n = int(input())
-p = list(map(int, input().split()))
-s = list(map(int, input().split()))
+now = list(input().split())
+rule = list(map(int, input().split()))
+dummy = [now]
+ans = ['0', '1', '2'] * (n//3)
+cnt = 0
+if now == ans:
+    print(0)
+else:
+    a = 0
+    while not a:
+        a, now = change()
+        cnt += 1
+        if now in dummy:
+            cnt = -1
+            break
+        dummy.append(now)
+    print(cnt)
--- baekjoon/2206.py
+++ baekjoon/2206.py
@@ -4,40 +4,33 @@ from collections import deque
 def bfs():
     way = [[1, 0], [-1, 0], [0, 1], [0, -1]]
     q = deque()
-    visit = [[0 for _ in range(m)] for _ in range(n)]
-    q.append([0, 0, 1])
+    q.append([0, 0, 1, 1])
     visit[0][0] = 1
     while q:
         v = q.popleft()
         for i in way:
-            nv = [v[0]+i[0], v[1]+i[1], v[2]+1]
-            if 0 <= nv[0] < n and 0 <= nv[1] < m and not visit[nv[0]][nv[1]] and lst[nv[0]][nv[1]] != 1:
-                q.append(nv)
-                visit[nv[0]][nv[1]] = 1
-                if [nv[0], nv[1]] == [n-1, m-1]:
-                    return nv[2]
-    return 0
+            nv = [v[0]+i[0], v[1]+i[1], v[2]+1, v[3]]
+            if v[3]:
+                if 0 <= nv[0] < n and 0 <= nv[1] < m and not visit[nv[0]][nv[1]]:
+                    if lst[nv[0]][nv[1]] == 1:
+                        if v[3]:
+                            nv[3] = 0
+                        else:
+                            continue
+                    q.append(nv)
+                    visit[nv[0]][nv[1]] = 1
+                    if [nv[0], nv[1]] == [n - 1, m - 1]:
+                        return nv[2]
+            else:
+                pass
+
+    return -1
 
 
 n, m = map(int, input().split())
 lst = list(list(map(int, input())) for _ in range(n))
-wall = []
-for i in range(n):
-    for j in range(m):
-        if lst[i][j]:
-            wall.append([i, j])
+visit = [[0 for _ in range(m)] for _ in range(n)]
+visit1 = [[0 for _ in range(m)] for _ in range(n)]
+print(bfs())
 
-mn = 10**6
-dummy = 0
-for i in wall:
-    lst[i[0]][i[1]] = 0
-    d = bfs()
-    if d:
-        if d < mn:
-            mn = d
-    lst[i[0]][i[1]] = 1
-
-if mn == 10**6:
-    print(-1)
-else:
-    print(mn)
+# 18% 틀림
