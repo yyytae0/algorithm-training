b376b67a6e195d3c7278195b50e9c252c05fcd02
yyytae0 <yyytae0@gmail.com>
2023-03-22 17:40:27 +0900

    이분탐색

--- baekjoon/3079.py
+++ baekjoon/3079.py
@@ -1,20 +1,21 @@
-n, m = map(int, input().split())
-# lst = list(int(input()) for _ in range(n))
-dp = []
-for i in range(n):
-    a = int(input())
-    dp.append([a, 0])
-dp.sort()
-for i in range(m):
-    d = dp[0][1] + dp[0][0]
-    for j in range(1, n):
-        if dp[j][1] + dp[j][0] < d:
-            dp[j][1] += dp[j][0]
-            break
+from sys import stdin
+
+
+def check():
+    d = 0
+    for i in lst:
+        d += mid//i
+    return d
+
+
+n, m = map(int, stdin.readline().split())
+lst = list(int(stdin.readline()) for _ in range(n))
+s, g = 0, min(lst)*m
+while s != g:
+    mid = (s + g)//2
+    d = check()
+    if d >= m:
+        g = mid
     else:
-        dp[0][1] += dp[0][0]
-mx = 0
-for i in dp:
-    if i[1] > mx:
-        mx = i[1]
-print(mx)
+        s = mid+1
+print(s)
