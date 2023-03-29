9c51814fb86ad9d9109b09592dfaa75ad6146eae
yyytae0 <yyytae0@gmail.com>
2023-03-29 17:49:16 +0900

    cpp 기초 연습

---
+++ c/Project1/Project1/13909.cpp
@@ -0,0 +1,22 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+	long long int n;
+	int cnt=0;
+	cin >> n;
+	for (int i = 1; i < n+1; i++)
+	{
+		if (i*i <= n)
+		{
+			cnt++;
+		}
+		else
+		{
+			break;
+		}
+	}
+	cout << cnt;
+}
+*/
--- c/Project1/Project1/train.cpp
+++ c/Project1/Project1/train.cpp
@@ -1,45 +1,6 @@
 #include <iostream>
 using namespace std;
-int prime[1000001] = {};
 
 int main() {
-	for (int i = 4; i < 1000001; i=i+2)
-	{
-		prime[i] = 1;
-	}
-	for (int i = 3; i < 1001; i=i+2)
-	{
-		if (prime[i] == 0)
-		{
-			for (int j = 3; j < 500001; j++)
-			{
-				if (i*j > 1000000)
-				{
-					break;
-				}
-				else
-				{
-					prime[i*j] = 1;
-				}
-			}
-		}
-	}
-	int ip, n;
-	cin >> ip;
-	for (int i = 0; i < ip; i++)
-	{
-		cin >> n;
-		int cnt = 0;
-		for (int i = 2; i < n/2+1; i++)
-		{
-			if (prime[i] == 0)
-			{
-				if (prime[n-i] == 0)
-				{
-					cnt++;
-				}
-			}
-		}
-		cout << cnt << endl;
-	}
+	return 0;
 }
