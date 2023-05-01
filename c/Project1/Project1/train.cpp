55643d4bb0f7f0deac2f842a4e5fd20e5a832668
yyytae0 <yyytae0@gmail.com>
2023-05-01 16:42:32 +0900

    .

--- c/Project1/Project1/train.cpp
+++ c/Project1/Project1/train.cpp
@@ -1,78 +1,12 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
 using namespace std;
-string ntobi(int);
-int check(string);
 
-int solution(int n) {
-	int answer = 0;
-	string s;
-	s = ntobi(n);
-	return check(s);
-}
+int solution(int n)
+{
+	int ans = 0;
 
-string ntobi(int n) {
-	string s = "";
-	while (true)
-	{
-		if (n == 1)
-		{
-			s = "1" + s;
-			break;
-		}
-		int d = n % 2;
-		s = to_string(d) + s;
-		n = n / 2;
-	}
-	return s;
-}
+	// [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+	cout << "Hello Cpp" << endl;
 
-int check(string s) {
-	int cnt1 = 0, cnt0 = 0, idx = 0, idx0 = 0, d = 0;
-	cout << s << endl;
-	for (auto i : s)
-	{
-		if (i == '1')
-		{
-			cnt1++;
-		}
-		else
-		{
-			cnt0++;
-			d = idx0;
-			idx0 = idx;
-		}
-		idx++;
-	}
-	if (cnt0 == 0 || (cnt0 == 1 && idx0 + 1 == s.length()))
-	{
-		string ans = "10";
-		for (int i = 0; i < cnt1 - 1; i++)
-		{
-			ans += "1";
-		}
-	}
-	else if(idx0 + 1 == s.length()) {
-		s.replace(d, 2, "10");
-	}
-	else {
-		s.replace(idx0, 2, "10");
-	}
-	int now = 1, ans = 0;
-	cout << s.length() << endl;
-	cout << s << endl;
-	for (int i = s.length() - 1; i >= 0; i--)
-	{
-		cout << s[i];
-		// int n = stoi(dummy) * now;
-		now *= 2;
-		// ans += num;
-	}
 	return ans;
-}
-
-int main() {
-	cout << endl << solution(78);
 }
